9a887226935267e67b4ac86925ed6db762442ff6
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-12-19 14:13:15 -0300

    Add Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function Navbar() {
+    return (
+        <nav className="main-navbar">
+            <a href="home" className='logo-text'>JDKClean</a>
+        </nav>
+    );
+}
